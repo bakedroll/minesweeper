a541aa3aca574527bb223f1753f0882128c1657c
bakedroll <46258197+bakedroll@users.noreply.github.com>
2020-05-31 13:26:11 +0200

    localization

---
+++ translations/QtMinesweeper_en.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>HighScore</name>
+    <message>
+        <location filename="../src/HighScore.cpp" line="149"/>
+        <source>Highscore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="150"/>
+        <source>Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="151"/>
+        <source>Total score</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="152"/>
+        <source>3BV score (min clicks)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="153"/>
+        <source>Clicks</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="154"/>
+        <source>Time</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="155"/>
+        <source>Difficulty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="157"/>
+        <source>Highscore TOP %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="168"/>
+        <source>Beginner</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="170"/>
+        <source>Intermediate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="172"/>
+        <source>Expert</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/HighScore.cpp" line="174"/>
+        <source>Custom game</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/MainWindow.ui" line="23"/>
+        <source>Minesweeper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="191"/>
+        <source>x</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="217"/>
+        <source>Mines:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="271"/>
+        <source>Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="281"/>
+        <source>Show Highscore...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.ui" line="286"/>
+        <source>Exit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="572"/>
+        <source>Highscore</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/MainWindow.cpp" line="572"/>
+        <source>Please enter your name for the high score:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
